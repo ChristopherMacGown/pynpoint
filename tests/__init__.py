f8a3b38f74a48ac0b4122fa2541630486f1a1f8f
Christopher MacGown <chris@slicehost.com>
2010-11-01 10:22:48 +0100

    Remove the * import from tests/__init__.py to make pylint happier

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 """ pynpoint tests """
 
-from tests.config_unittest import *
-from tests.protocol_unittest import *
-from tests.redis_unittest import *
+from tests.config_unittest import ConfigTestCase
+from tests.protocol_unittest import ProtocolTestCase
+from tests.redis_unittest import RedisTestCase
